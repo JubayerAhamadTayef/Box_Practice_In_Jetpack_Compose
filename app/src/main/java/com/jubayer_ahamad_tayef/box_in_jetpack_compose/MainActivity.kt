d990530bf3bafe877dfe89558354beac7a605770
JubayerAhamadTayef <jubayerahmedtayef.579462@gmail.com>
2025-01-18 16:34:00 +0600

    Practice Box with Spacer in Jetpack Compose!

--- app/src/main/java/com/jubayer_ahamad_tayef/box_in_jetpack_compose/MainActivity.kt
+++ app/src/main/java/com/jubayer_ahamad_tayef/box_in_jetpack_compose/MainActivity.kt
@@ -4,13 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.jubayer_ahamad_tayef.box_in_jetpack_compose.ui.theme.Box_In_Jetpack_ComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,10 +26,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             Box_In_Jetpack_ComposeTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+
                 }
             }
         }
@@ -31,17 +34,42 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MyBox(modifier: Modifier) {
+    Box(modifier = modifier.fillMaxSize(), ) {
+        Spacer(
+            modifier = modifier
+                .size(100.dp)
+                .background(MaterialTheme.colorScheme.primary)
+        )
+        Spacer(
+            modifier = modifier
+                .size(100.dp)
+                .background(MaterialTheme.colorScheme.primary)
+                .align(alignment = Alignment.Center)
+        )
+        Spacer(
+            modifier = modifier
+                .size(100.dp)
+                .background(MaterialTheme.colorScheme.primary)
+                .align(Alignment.BottomEnd)
+        )
+        Spacer(
+            modifier = modifier
+                .size(100.dp)
+                .background(MaterialTheme.colorScheme.primary)
+                .align(Alignment.TopEnd)
+        )
+        Spacer(
+            modifier = modifier
+                .size(100.dp)
+                .background(MaterialTheme.colorScheme.primary)
+                .align(Alignment.BottomStart)
+        )
+    }
 }
 
-@Preview(showBackground = true)
+@Preview(showSystemUi = true)
 @Composable
-fun GreetingPreview() {
-    Box_In_Jetpack_ComposeTheme {
-        Greeting("Android")
-    }
+fun MyBoxPreview() {
+    MyBox(modifier = Modifier.padding(5.dp))
 }
